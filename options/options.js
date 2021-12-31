b10cfeb8e189c8cd146c014c03f9ea663c4be0ce
Kushal Karki <nepalkushal1995@outlook.com>
2021-12-31 15:09:11 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "kus_local_react_nati_7526",
+  url: "https://kus_local_react_nati_7526.botics.co",
+  api: "https://kus_local_react_nati_7526.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
